8f1f6725e9ffb3e478e934a559f171468cf0d9d2
StivenJM <stevenmoposita456@gmail.com>
2024-09-12 10:53:10 -0500

    Change position of Tutorial button

--- app/src/main/java/com/example/handappmobile_epn/navigation/AppScreens.kt
+++ app/src/main/java/com/example/handappmobile_epn/navigation/AppScreens.kt
@@ -10,7 +10,6 @@ sealed class AppScreens(
 ) {
     object HomeScreen : AppScreens("Inicio", R.drawable.app_selected_home, R.drawable.app_unselected_home, "home_screen")
     object DevicesScreen : AppScreens("Dispositivos", R.drawable.app_selected_bt, R.drawable.app_unselected_bt, "devices_screen")
-    object TutorialScreen : AppScreens("Tutorial", R.drawable.app_selected_tutorial, R.drawable.app_unselected_tutorial, "tutorial_screen")
     object SettingsScreen : AppScreens("Ajustes", R.drawable.app_selected_settings, R.drawable.app_unselected_settings, "settings_screen")
     object AboutScreen : AppScreens("Acerca de", R.drawable.app_selected_about, R.drawable.app_unselected_about, "about_screen")
 }
