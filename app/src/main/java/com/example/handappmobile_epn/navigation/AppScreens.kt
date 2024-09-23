b2a140b0a20100210b316d35f3869e849c244c5d
StivenJM <stevenmoposita456@gmail.com>
2024-09-22 19:43:42 -0500

    Using debug icons

--- app/src/main/java/com/example/handappmobile_epn/navigation/AppScreens.kt
+++ app/src/main/java/com/example/handappmobile_epn/navigation/AppScreens.kt
@@ -10,7 +10,7 @@ sealed class AppScreens(
 ) {
     object HomeScreen : AppScreens("Inicio", R.drawable.app_selected_home, R.drawable.app_unselected_home, "home_screen")
     object DevicesScreen : AppScreens("Dispositivos", R.drawable.app_selected_bt, R.drawable.app_unselected_bt, "devices_screen")
-    object DebugScreen : AppScreens("Depuración", R.drawable.app_selected_bt, R.drawable.app_unselected_bt, "debug_screen")
+    object DebugScreen : AppScreens("Depuración", R.drawable.app_selected_debug, R.drawable.app_unselected_debug, "debug_screen")
     object SettingsScreen : AppScreens("Ajustes", R.drawable.app_selected_settings, R.drawable.app_unselected_settings, "settings_screen")
     object AboutScreen : AppScreens("Acerca de", R.drawable.app_selected_about, R.drawable.app_unselected_about, "about_screen")
 }
