4f391b726702596fa52f2d870f68fc32658adea8
StivenJM <stevenmoposita456@gmail.com>
2024-09-29 22:05:01 -0500

    Fixed finger images positioning in the HandController component

--- app/src/main/java/com/example/handappmobile_epn/ui/components/HandController.kt
+++ app/src/main/java/com/example/handappmobile_epn/ui/components/HandController.kt
@@ -1,9 +1,10 @@
 package com.example.handappmobile_epn.ui.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
@@ -15,13 +16,18 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.handappmobile_epn.R
+import com.example.handappmobile_epn.bt.BluetoothConnectionManager
+import com.example.handappmobile_epn.ui.screen.HomeScreen
 
 @Composable
 fun HandController(
@@ -37,28 +43,37 @@ fun HandController(
     Box(
         modifier = Modifier
             .fillMaxWidth()
-            .height(400.dp)
+            .height(400.dp),
+        contentAlignment = Alignment.Center
     ) {
-        // Imagen de la mano
-        Image(
-            painter = painterResource(id = R.drawable.mano),
-            contentDescription = "Mano en blanco y negro",
-            modifier = Modifier.fillMaxSize()
-        )
+        Box(
+            modifier = Modifier
+                .fillMaxHeight()
+        ) {
+            // Imagen de la mano
+            Image(
+                painter = painterResource(id = R.drawable.mano),
+                contentDescription = "Mano en blanco y negro",
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .background(Color.Red),
+                contentScale = ContentScale.FillHeight
+            )
 
-        // Imágenes condicionales de los dedos
-        FingerImages(
-            estaPulsadoPulgarSuperior,
-            estaPulsadoPulgarInferior,
-            estaPulsadoIndice,
-            estaPulsadoMedio,
-            estaPulsadoAnular,
-            estaPulsadoMenique
-        )
+            // Imágenes condicionales de los dedos
+            FingerImages(
+                estaPulsadoPulgarSuperior,
+                estaPulsadoPulgarInferior,
+                estaPulsadoIndice,
+                estaPulsadoMedio,
+                estaPulsadoAnular,
+                estaPulsadoMenique
+            )
 
-        // Botones de los dedos
-        if (habilitar) {
-            HandButtons(onDedoPulsado)
+            // Botones de los dedos
+            if (habilitar) {
+                HandButtons(onDedoPulsado)
+            }
         }
     }
 }
@@ -72,12 +87,12 @@ fun FingerImages(
     estaPulsadoAnular: Boolean,
     estaPulsadoMenique: Boolean
 ) {
-    MostrarImagen(estaPulsadoPulgarSuperior, R.drawable.pulgarsuperiorverde, 264.dp, 175.dp, 103.dp, 103.dp)
-    MostrarImagen(estaPulsadoPulgarInferior, R.drawable.pulgarinferiorverde, 193.dp, 245.dp, 120.dp, 120.dp)
-    MostrarImagen(estaPulsadoIndice, R.drawable.indiceverde, 129.dp, 19.dp, 245.dp, 245.dp)
-    MostrarImagen(estaPulsadoMedio, R.drawable.medioverde, 53.dp, -8.dp, 258.dp, 258.dp)
-    MostrarImagen(estaPulsadoAnular, R.drawable.anularverde, 10.dp, 27.dp, 215.dp, 215.dp)
-    MostrarImagen(estaPulsadoMenique, R.drawable.meniqueverde, -12.dp, 89.dp, 188.dp, 188.dp)
+    MostrarImagen(estaPulsadoPulgarSuperior, R.drawable.pulgarsuperiorverde, 269.dp, 175.dp, 103.dp, 103.dp)
+    MostrarImagen(estaPulsadoPulgarInferior, R.drawable.pulgarinferiorverde, 196.dp, 247.dp, 120.dp, 120.dp)
+    MostrarImagen(estaPulsadoIndice, R.drawable.indiceverde, 133.dp, 17.dp, 245.dp, 245.dp)
+    MostrarImagen(estaPulsadoMedio, R.drawable.medioverde, 55.dp, (-11).dp, 258.dp, 258.dp)
+    MostrarImagen(estaPulsadoAnular, R.drawable.anularverde, 11.dp, 25.dp, 215.dp, 215.dp)
+    MostrarImagen(estaPulsadoMenique, R.drawable.meniqueverde, (-12).dp, 89.dp, 188.dp, 188.dp)
 }
 
 @Composable
@@ -93,10 +108,10 @@ fun LogicaBotonesMano(onDedoPulsado: (String, Boolean) -> Unit) {
     // Botones y lógica de HandiEpn aquí
     BotonDedo("Pulgar Superior", 270.dp, 210.dp, -45f, 90.dp, 40.dp, onDedoPulsado)
     BotonDedo("Pulgar Inferior", 210.dp, 280.dp, -45f, 90.dp, 40.dp, onDedoPulsado)
-    BotonDedo("Índice", 140.dp, 120.dp, -70f, 200.dp, 35.dp, onDedoPulsado)
+    BotonDedo("Índice", 140.dp, 120.dp, -72f, 210.dp, 40.dp, onDedoPulsado)
     BotonDedo("Medio", 60.dp, 90.dp, 90f, 230.dp, 35.dp, onDedoPulsado)
     BotonDedo("Anular", 10.dp, 90.dp, 75f, 200.dp, 35.dp, onDedoPulsado)
-    BotonDedo("Meñique", (-20).dp, 140.dp, 55f, 160.dp, 35.dp, onDedoPulsado)
+    BotonDedo("Meñique", (-20).dp, 140.dp, 55f, 155.dp, 35.dp, onDedoPulsado)
 }
 
 /* Función para declarar los datos de los botones de los dedos */
@@ -129,4 +144,10 @@ fun BotonDedo(
     ) {
         /* El botón es invisible, pero realiza la acción de alternar el estado */
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    HomeScreen(BluetoothConnectionManager(null))
 }
--- app/src/main/java/com/example/handappmobile_epn/ui/components/ImageComponent.kt
+++ app/src/main/java/com/example/handappmobile_epn/ui/components/ImageComponent.kt
@@ -1,10 +1,12 @@
 package com.example.handappmobile_epn.ui.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.Dp
 
